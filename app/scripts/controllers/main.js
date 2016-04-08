7e653110ddcb376d61ca56cc00519c7ccbb292b8
Charles Lambert <c.lambert1213@gmail.com>
2016-04-08 11:33:38 -0700

    base setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name softwareEngineeringTeamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the softwareEngineeringTeamApp
+ */
+angular.module('softwareEngineeringTeamApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
