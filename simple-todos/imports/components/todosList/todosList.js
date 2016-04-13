89b3b1df9b87bbe861ad557fa7d92b087e4ca28f
phil0s0pher <c.lambert1213@gmail.com>
2016-04-13 15:33:35 -0700

    db

--- simple-todos/imports/components/todosList/todosList.js
+++ simple-todos/imports/components/todosList/todosList.js
@@ -1,77 +1,70 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import {
-    Meteor
+  Meteor
 } from 'meteor/meteor';
 import {
-    Tasks
+  Tasks
 } from '../../api/tasks.js';
 import template from './todosList.html';
 
 class TodosListCtrl {
-    constructor($scope) {
-        $scope.viewModel(this);
-        this.hideCompleted = false;
+  constructor($scope) {
+    $scope.viewModel(this);
+    this.subscribe('tasks');
+    this.hideCompleted = false;
 
-        this.helpers({
-                tasks() {
-                    const selector = {};
+    this.helpers({
+      tasks() {
+        const selector = {};
 
-                    // If hide completed is checked, filter tasks
-                    if (this.getReactively('hideCompleted')) {
-                        selector.checked = {
-                            $ne: true
-                        };
-                    }
-
-                    // Show newest tasks at the top
-                    return Tasks.find({}, {
-                        sort: {
-                            createdAt: -1
-                        }
-                    });
-                },
-                incompleteCount() {
-                    return Tasks.find({
-                        checked: {
-                            $ne: true
-                        }
-                    }).count();
-                },
-                currentUser() {
-                    return Meteor.user();
-                })
-        }
-        addTask(newTask) {
-            // Insert a task into the collection
-            Tasks.insert({
-                text: newTask,
-                createdAt: new Date,
-                owner: Meteor.userId(),
-                username: Meteor.user().username
-            });
-
-            // Clear form
-            this.newTask = '';
-        }
-        setChecked(task) {
-            // Set the checked property to the opposite of its current value
-            Tasks.update(task._id, {
-                $set: {
-                    checked: !task.checked
-                }
-            });
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
         }
 
-        removeTask(task) {
-            Tasks.remove(task._id);
-        }
-    }
-
-    export default angular.module('todosList', [
-            angularMeteor
-        ])
-        .component('todosList', {
-            templateUrl: 'imports/components/todosList/todosList.html',
-            controller: ['$scope', TodosListCtrl]
+        // Show newest tasks at the top
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
         });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser() {
+        return Meteor.user();
+      }
+    });
+  }
+  addTask(newTask) {
+    // Insert a task into the collection
+    Meteor.call('tasks.insert', newTask);
+
+    // Clear form
+    this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
+  }
+
+  removeTask(task) {
+    Meteor.call('tasks.remove', task._id);
+  }
+}
+export default angular.module('todosList', [
+    angularMeteor
+  ])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
